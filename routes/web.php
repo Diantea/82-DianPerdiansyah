aa61fc81ed5966ebb137eabf2885e57ccad7bf66
dian <belajar1321@gmail.com>
2024-05-29 14:18:53 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\WithAuth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,18 +15,20 @@ Route::get('/', function () {
     }
 });
 
-
-Route::get('pending', [\App\Http\Controllers\DashboardController::class, 'pending'])->name('pending');
-Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-Route::resource('/information', \App\Http\Controllers\InformationController::class);
-Route::resource('/registration', \App\Http\Controllers\RegistrationController::class);
-Route::get('/internship/submission', [\App\Http\Controllers\InternshipController::class, 'index_submission'])->name('internship.index_submission');
-Route::resource('/internship', \App\Http\Controllers\InternshipController::class);
-Route::resource('/schedule', \App\Http\Controllers\ScheduleController::class);
-Route::resource('/teacher', \App\Http\Controllers\TeacherController::class);
-Route::resource('/report', \App\Http\Controllers\ReportController::class);
-Route::resource('/user', \App\Http\Controllers\UserController::class);
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware([WithAuth::class])->group(function(){
+
+    Route::get('pending', [\App\Http\Controllers\DashboardController::class, 'pending'])->name('pending');
+    Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/information', \App\Http\Controllers\InformationController::class);
+    Route::resource('/registration', \App\Http\Controllers\RegistrationController::class);
+    Route::get('/internship/submission', [\App\Http\Controllers\InternshipController::class, 'index_submission'])->name('internship.index_submission');
+    Route::resource('/internship', \App\Http\Controllers\InternshipController::class);
+    Route::resource('/schedule', \App\Http\Controllers\ScheduleController::class);
+    Route::resource('/teacher', \App\Http\Controllers\TeacherController::class);
+    Route::resource('/report', \App\Http\Controllers\ReportController::class);
+    Route::resource('/user', \App\Http\Controllers\UserController::class);
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
